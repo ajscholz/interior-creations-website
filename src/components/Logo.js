ee41355cdc92c013237a2da1dec7fd1424ff09d2
Andrew Scholz <yoitsscholzy@gmail.com>
2019-09-05 16:43:02 -0400

    add logo component

---
+++ src/components/Logo.js
@@ -0,0 +1,20 @@
+import React from "react"
+import Img from "gatsby-image"
+import { graphql, useStaticQuery } from "gatsby"
+
+const Logo = () => {
+  const { image } = useStaticQuery(logo)
+  return <Img fluid={image.fluid} />
+}
+
+export default Logo
+
+export const logo = graphql`
+  {
+    image: contentfulAsset(title: { eq: "Interior Creations Logo" }) {
+      fluid {
+        ...GatsbyContentfulFluid
+      }
+    }
+  }
+`
