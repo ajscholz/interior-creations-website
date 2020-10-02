f17723996940a436a63a34011623673cc11a53bc
ajscholz <yoitsscholzy@gmail.com>
2020-10-02 13:23:21 -0400

    update background parallax images to gatsby-background-image

--- src/components/Parallax.js
+++ src/components/Parallax.js
@@ -1,11 +1,13 @@
+import BackgroundImage from "gatsby-background-image"
 import React from "react"
 
 const Parallax = ({ image }) => {
   return (
-    <div
+    <BackgroundImage
+      fluid={image.fluid}
       style={{
         height: "500px",
-        backgroundImage: `url(${image.fluid.src})`,
+        // backgroundImage: `url(${image.fluid.src})`,
         backgroundAttachment: "fixed",
         backgroundPosition: "center",
         backgroundRepeat: "no-repeat",
